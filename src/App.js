5094c3474e70bc28ce8e2dd3a0f439d78b970a4e
naMqe <naMqe07@gmail.com>
2022-01-03 23:11:12 +0100

    added alert context and reducer

--- src/App.js
+++ src/App.js
@@ -5,24 +5,27 @@ import NotFound from './pages/NotFound'
 import Footer from './components/layout/Footer'
 import Navbar from './components/layout/Navbar'
 import { GithubProvider } from './context/github/GithubContext'
+import { AlertProvider } from './context/alert/AlertContext'
 
 function App() {
   return (
     <GithubProvider>    
-      <BrowserRouter>
-        <div className="flex flex-col justify-between h-screen">
-          <Navbar />
+      <AlertProvider>
+        <BrowserRouter>
+          <div className="flex flex-col justify-between h-screen">
+            <Navbar />
 
-          <main className='container mx-auto px-3 pb-12'>
-            <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/about' element={<About />} />
-              <Route path='/*' element={<NotFound />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </BrowserRouter>
+            <main className='container mx-auto px-3 pb-12'>
+              <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/*' element={<NotFound />} />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </BrowserRouter>
+      </AlertProvider>
     </GithubProvider>
   );
 }
