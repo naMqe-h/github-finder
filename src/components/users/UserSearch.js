6d54f2d8d4964e1792c8589e0c5718baa27831b3
naMqe <naMqe07@gmail.com>
2022-01-03 23:34:44 +0100

    added alert component

--- src/components/users/UserSearch.js
+++ src/components/users/UserSearch.js
@@ -1,9 +1,11 @@
 import { useState, useContext } from "react"
 import GithubContext from "../../context/github/GithubContext"
+import AlertContext from "../../context/alert/AlertContext"
 
 export default function UserSearch() {
     const [text, setText] = useState('')
     const { users, searchUsers, clearUsers } = useContext(GithubContext)
+    const { setAlert } = useContext(AlertContext)
 
     const handleChange = (e) => setText(e.target.value)
 
@@ -11,7 +13,7 @@ export default function UserSearch() {
         e.preventDefault()
 
         if(text === '') {
-            alert('Please enter something')
+            setAlert('Please enter something', 'error')
         } else {
             searchUsers(text)
             setText('')
