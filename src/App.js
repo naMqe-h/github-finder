bd3cdd850fc92bd2278a3103a6a1986c3a10bcb7
naMqe <naMqe07@gmail.com>
2022-01-03 21:25:37 +0100

    added GithubContext

--- src/App.js
+++ src/App.js
@@ -2,25 +2,28 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import NotFound from './pages/NotFound'
-import Footer from './components/layout/Footer';
-import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer'
+import Navbar from './components/layout/Navbar'
+import { GithubProvider } from './context/github/GithubContext'
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="flex flex-col justify-between h-screen">
-        <Navbar />
+    <GithubProvider>    
+      <BrowserRouter>
+        <div className="flex flex-col justify-between h-screen">
+          <Navbar />
 
-        <main className='container mx-auto px-3 pb-12'>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/*' element={<NotFound />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </BrowserRouter>
+          <main className='container mx-auto px-3 pb-12'>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/about' element={<About />} />
+              <Route path='/*' element={<NotFound />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </BrowserRouter>
+    </GithubProvider>
   );
 }
 
--- src/components/users/UserResult.js
+++ src/components/users/UserResult.js
@@ -1,25 +1,13 @@
-import { useEffect, useState } from "react"
+import { useEffect, useContext } from "react"
 import UserItem from "./UserItem"
+import GithubContext from "../../context/github/GithubContext"
 
 export default function UserResult() {
-    const [users, setUsers] = useState([])
-    const [loading, setLoading] = useState(true)
+    const { users, loading, fetchUsers } = useContext(GithubContext)
 
     useEffect(() => {
         fetchUsers()
     }, [])
-
-    const fetchUsers = async () => {
-        const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
-            headers: {
-                Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-            },
-        })
-
-        const data = await response.json()
-        setUsers(data)
-        setLoading(false)
-    }
 
     return !loading ? (
         <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
